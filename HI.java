2b774a11fb8798a8b02c7f0286fa07f586f5f096
Arjoo-Coder <arjoo1793@gmail.com>
2026-08-01 08:30:38 +0530

    6th day

--- HI.java
+++ HI.java
@@ -3,7 +3,6 @@ public class HI {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
-        System.out.println("Hello World");
 
     }
 }
